42eb7d3b4c4d9b50a5c3b10ce28553c12cd26c65
Rahul Patil <rahulg@traverseinformatics.com>
2024-08-02 16:24:09 +0530

    Check On Web

--- lib/feature/item_list/item_list.dart
+++ lib/feature/item_list/item_list.dart
@@ -32,88 +32,100 @@ class _ListOfItemState extends State<ListOfItem> {
       body: Consumer<CreateItemViewModel>(
         builder: (context, value, child) {
           final itemData = value;
-          return ListView.builder(
-            shrinkWrap: true,
-            itemCount: itemData.items.length,
-            itemBuilder: (context, index) {
-              final item = itemData.items[index];
-              return Container(
-                margin: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      margin:
-                          const EdgeInsets.only(left: 20, top: 20, bottom: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            item.name,
-                            style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 19,
-                                fontWeight: FontWeight.w700),
-                          ),
-                          Text(
-                            item.description,
-                            style: TextStyle(
-                                color: Colors.black.withOpacity(0.5),
-                                fontSize: 17,
-                                fontWeight: FontWeight.w700),
-                          ),
-                        ],
+          return value.items.isEmpty
+              ? const Center(
+                  child: Text(
+                    'Currently, no items found at the moment.',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 19,
+                        fontWeight: FontWeight.w700),
+                  ),
+                )
+              : ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: itemData.items.length,
+                  itemBuilder: (context, index) {
+                    final item = itemData.items[index];
+                    return Container(
+                      margin: const EdgeInsets.only(
+                          left: 20, right: 20, bottom: 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(),
                       ),
-                    ),
-                    Container(
-                      margin:
-                          const EdgeInsets.only(right: 20, top: 20, bottom: 20),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(context, MaterialPageRoute(
-                                builder: (context) {
-                                  return EditItem(
-                                    itemName: item.name,
-                                  );
-                                },
-                              ));
-                            },
-                            child: const Icon(
-                              Icons.edit,
+                          Container(
+                            margin: const EdgeInsets.only(
+                                left: 20, top: 20, bottom: 20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  item.name,
+                                  style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 19,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                                Text(
+                                  item.description,
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.5),
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                              ],
                             ),
                           ),
-                          const SizedBox(
-                            width: 15,
-                          ),
-                          GestureDetector(
-                            onTap: () async {
-                              context.loaderOverlay.show();
-                              await Future.delayed(const Duration(seconds: 2))
-                                  .then((value) {
-                                context.loaderOverlay.hide();
-                                itemData.removeItem(item);
-                                showSuccessSnackBar(context,
-                                    'Item ${item.name} deleted successfully.');
-                              });
-                            },
-                            child: const Icon(
-                              Icons.delete,
-                              color: Colors.red,
+                          Container(
+                            margin: const EdgeInsets.only(
+                                right: 20, top: 20, bottom: 20),
+                            child: Row(
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(context, MaterialPageRoute(
+                                      builder: (context) {
+                                        return EditItem(
+                                          itemName: item.name,
+                                        );
+                                      },
+                                    ));
+                                  },
+                                  child: const Icon(
+                                    Icons.edit,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 15,
+                                ),
+                                GestureDetector(
+                                  onTap: () async {
+                                    context.loaderOverlay.show();
+                                    await Future.delayed(
+                                            const Duration(seconds: 2))
+                                        .then((value) {
+                                      context.loaderOverlay.hide();
+                                      itemData.removeItem(item);
+                                      showSuccessSnackBar(context,
+                                          'Item ${item.name} deleted successfully.');
+                                    });
+                                  },
+                                  child: const Icon(
+                                    Icons.delete,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                              ],
                             ),
-                          ),
+                          )
                         ],
                       ),
-                    )
-                  ],
-                ),
-              );
-            },
-          );
+                    );
+                  },
+                );
         },
       ),
     );
