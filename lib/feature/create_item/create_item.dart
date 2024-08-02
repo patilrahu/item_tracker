917be9d262ff596625361b9bc848142524669dd7
Rahul Patil <rahulg@traverseinformatics.com>
2024-08-02 12:24:55 +0530

    flutter setup added

---
+++ lib/feature/create_item/create_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateItem extends StatefulWidget {
+  const CreateItem({super.key});
+
+  @override
+  State<CreateItem> createState() => _CreateItemState();
+}
+
+class _CreateItemState extends State<CreateItem> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
